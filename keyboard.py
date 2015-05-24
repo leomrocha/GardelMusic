c7388bf3c1bceca4008f07b5b025c83ca8cff034
Leonardo M. Rocha <leo.m.rocha@gmail.com>
2015-05-24 15:01:36 +0200

    comments cleanup

--- keyboard.py
+++ keyboard.py
@@ -23,22 +23,3 @@ class Keyboard(pygame.sprite.Group):
         self.add(self.kb)        
         self.draw()
         
-        '''
-        #taken out for a simpler approach for a demo, will only be done with an image with a Surface
-        #Load Keyboard images
-        self.background = 
-        self.white_keys = 
-        self.black_keys = 
-        self.color_bar = 
-        
-        self.img_background = pygame.image.load("assets/images/keyboard/keyboard_background.png").convert_alpha()
-        self.img_white_keys = pygame.image.load("assets/images/keyboard/keyboard_white-keys.png").convert_alpha() 
-        self.img_black_keys = pygame.image.load("assets/images/keyboard/keyboard_black-keys.png").convert_alpha() 
-        self.img_color_bar = pygame.image.load("assets/images/keyboard/keyboard_color-bar.png").convert_alpha()
-        #when color bar will be separated for white and black keys
-        #color_bar_wk = 
-        #color_bar_bk = 
-        
-        self.
-        #END taken out for a simpler approach for a demo, will only be done with an image with a Surface
-        '''
