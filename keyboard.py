cfaa43b43d79faa6bc8ae38871e104fd7e3e531a
Leonardo M. Rocha <leo.m.rocha@gmail.com>
2015-05-24 18:10:31 +0200

    synesthesia and color conversion should be now resolved

--- keyboard.py
+++ keyboard.py
@@ -3,11 +3,56 @@ Keyboard Widget
 """
 import pygame
 
+import keyboard_mappings
+from synesthesia import color_map
+
+class KeySprite(pygame.sprite.Sprite):
+    """
+    Key sprite that
+    """
+    
+    def __init__(self, midi_id, pos, size):
+        """
+        midi_id = midi_id of the represented key
+        pos = position
+        size = size of the key
+
+        """
+        self.midi_id = midi_id
+        self.pos = pos
+        self.size = size
+        #if key is black
+        #self.image = 
+        #self.image_active = 
+
+
+class WhiteKeyGroup(pygame.sprite.Group):
+    """
+    """
+    #TODO
+    pass
+
+
+class BlackKeyGroup(pygame.sprite.Sprite):
+    """
+    """
+    #TODO
+    pass
+
+
+class KeyboardGroup(pygame.sprite.Group):
+    """
+    Piano keyboard 
+    is a piano keyboard implementation where everything is computer generated
+    """
+    #TODO
+    pass
 
 class KeyboardSprite(pygame.sprite.Sprite):
     """
     Piano Keyboard Sprite
     """
+    
     def __init__(self, pos=(0,0)):
         """
         """
@@ -16,8 +61,8 @@ class KeyboardSprite(pygame.sprite.Sprite):
         super(KeyboardSprite, self).__init__()
         #pygame.sprite.Sprite.__init__(self)
         #
-        
-        #self.kb = pygame.sprite.Sprite()
+
+        #Keyboard Image        
         self.image = pygame.image.load("assets/images/keyboard/plain_keyboard.png").convert_alpha()
         self.rect = self.image.get_rect() # use image extent values
         self.rect.topleft = pos
@@ -29,14 +74,28 @@ class KeyboardSprite(pygame.sprite.Sprite):
         super(KeyboardSprite, self).update()
         pass
         
-    #TODO on mouse down
-    #TODO on mouse up
-    #TODO on key down
-    #TODO on key up
-    #MIDI events
-    #TODO on key on
-    #TODO on key off
-
+    def collides_key(self, pos):
+        """
+        Calculates the key in the keyboard that is touched by the event position
+        returns:
+        midi_note (pygame event notation??? TODO decide)
+        bounding box (x,y,w,h) of the element
+        
+        """
+        #TODO
+        #1 see if collides with the keyboard, else return false
+        #2 if so, see what note (white) it collides, calculate also the rect containing the white note (this should be pre-calculated somewhere to make it faster)
+        
+        #3 if height < Ybk0 return true and note that it collides (midi notation) #Ybk0 == lower Y position of black key counted from the bottom
+        
+        #4 if heignt >Ybk0 see if it collides with which note (the flat or the sharp ...
+        
+        #5 return the nothe that it collides with, position, bounding box and midi notation
+        
+        pass
+        
+        
+        
 
 class Keyboard(pygame.sprite.Group):
     """
@@ -51,7 +110,30 @@ class Keyboard(pygame.sprite.Group):
         #
         self.kb_sprite = KeyboardSprite([0,400])
         self.add(self.kb_sprite)
+        
+        #Active Key Images (colors only)
+        #TODO
+        
+        #Active Finger Images (numbers 1-5)
+        #TODO
+        
+        #Arrows showing next key to press
+        #TODO
     
     def update(self):
         super(Keyboard, self).update()
+        
+    def on_mouse_down(self, pos):
+        """
+        """
+        pass
+    
+    def on_mouse_up(self, pos):
+        """
+        """
+        pass
+        
+    #MIDI events
+    #TODO on key on
+    #TODO on key off
 
---
+++ keyboard_mappings.py
@@ -0,0 +1,72 @@
+"""
+This file contains :
+midi id, number id, name id (number), associated color id, x, y, width, height
+
+x,y,width and height are given in 
+"""
+
+
+NAMES_ENGLISH = {
+    0:['C'],
+    1:['C#','Db'],
+    2:['D'],
+    3:['D#','Eb'],
+    4:['E'],
+    5:['F'],
+    6:['F#','Gb'],
+    7:['G'],
+    8:['G#','Ab'],
+    9:['A'],
+    10:['A#','Bb'],
+    11:['B'], 
+    }
+    
+
+NAMES_GERMAN = {
+    0:['C'],
+    1:['C#','Db'],
+    2:['D'],
+    3:['D#','Eb'],
+    4:['E'],
+    5:['F'],
+    6:['F#','Gb'],
+    7:['G'],
+    8:['G#','Ab'],
+    9:['A'],
+    10:['A#','Hb'],
+    11:['H'], 
+    }
+
+
+NAMES_LATIN = {
+    0:['DO'],
+    1:['DO#','REb'],
+    2:['RE'],
+    3:['RE#','MIb'],
+    4:['MI'],
+    5:['FA'],
+    6:['FA#','SOLb'],
+    7:['SOL'],
+    8:['SOL#','LAb'],
+    9:['LA'],
+    10:['LA#','SIb'],
+    11:['SI'],
+    }
+
+
+def get_octave(midi_id):
+    return int(midi_id / 12)
+
+    
+def get_note_name(midi_id, lang='latin'):
+    if lang == 'english':
+        return NAMES_ENGLISH[midi_id]
+    elif lang == 'german':
+        return NAMES_GERMAN[midi_id]
+    #by default asume note names are in latin
+    return NAMES_LATIN[midi_id]
+
+        
+keyboard_map = {
+
+}
