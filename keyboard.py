fcc735ea62f6cda2b99e3ade77c5005877be59e1
Leonardo M. Rocha <leo.m.rocha@gmail.com>
2015-05-24 15:42:11 +0200

    keyboard already on display

--- keyboard.py
+++ keyboard.py
@@ -1,25 +1,57 @@
-
+"""
+Keyboard Widget
+"""
 import pygame
 
 
-class Keyboard(pygame.sprite.Group):
+class KeyboardSprite(pygame.sprite.Sprite):
     """
-    Piano Keyboard
+    Piano Keyboard Sprite
     """
-    def __init__(self):
+    def __init__(self, pos=(0,0)):
         """
         """
         #Super constructor
         #super().__init__()
+        super(KeyboardSprite, self).__init__()
+        #pygame.sprite.Sprite.__init__(self)
         #
-        xpos = 0
-        ypos = 0
         
-        self.kb = pygame.sprite.Sprite()
-        self.kb.image = pygame.image.load("assets/images/keyboard/plain_keyboard.png").convert_alpha()
-        self.kb.rect = self.kb.image.get_rect() # use image extent values
-        self.kb.rect.topleft = [xpos, ypos]
+        #self.kb = pygame.sprite.Sprite()
+        self.image = pygame.image.load("assets/images/keyboard/plain_keyboard.png").convert_alpha()
+        self.rect = self.image.get_rect() # use image extent values
+        self.rect.topleft = pos
         
-        self.add(self.kb)        
-        self.draw()
+    def update(self):
+        """
+        """
+        #print "updating keyboard sprite"
+        super(KeyboardSprite, self).update()
+        pass
         
+    #TODO on mouse down
+    #TODO on mouse up
+    #TODO on key down
+    #TODO on key up
+    #MIDI events
+    #TODO on key on
+    #TODO on key off
+
+
+class Keyboard(pygame.sprite.Group):
+    """
+    """
+    def __init__(self, pos=(0,0)):
+        """
+        """
+        #Super constructor
+        #super().__init__()
+        super(Keyboard, self).__init__()
+        #pygame.sprite.Sprite.__init__(self)
+        #
+        self.kb_sprite = KeyboardSprite([0,450])
+        self.add(self.kb_sprite)
+    
+    def update(self):
+        super(Keyboard, self).update()
+
--- main.py
+++ main.py
@@ -8,25 +8,60 @@ from pygame.locals import *
 from keyboard import *
 
 pygame.init()
-screen = pygame.display.set_mode((1200, 800), RESIZABLE)
+screen = pygame.display.set_mode((1060, 800), RESIZABLE)
 
 clock = pygame.time.Clock() # create a clock object for timing
 
+def setup():
+    """
+    sets up many things needed previous the game starting
+    """
+    pass
 
+
+def teardown():
+    """
+    finishes everything that has to be ended before game exit
+    """
+    pass
+
+
+def process_event(event):
+    """
+    process pygame events (keyboard, mouse and other events)
+    """
+    pass
+    
+    
 def main():
     
     done = False
+    
+    #######################
+    keyboard = Keyboard()
+    
+    #######################
     while not done:
+        #process events
         for event in pygame.event.get():
-                if event.type == pygame.QUIT:
+            #check if finish
+            if event.type == pygame.QUIT:
                         done = True
-        keyboard = Keyboard()
-        clock.tick(120)
-        pygame.display.flip()
+            #
+            process_event(event)
+            
+        keyboard.update()
+        keyboard.draw(screen)
         
+        
+        pygame.display.flip()
+        clock.tick(120)
+    
 
 if __name__=="__main__":
+    setup()
     main()
+    teardown()
     pygame.quit()
     sys.exit()
 
